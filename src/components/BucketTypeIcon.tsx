b3dce7c578525691e94a50422ba8011364f0b5cb
Gregoire Pailler <gregoire@pailler.fr>
2022-02-09 10:30:38 +0800

    Introduce community bucket with stars display in BucketTypeIcon

--- src/components/BucketTypeIcon.tsx
+++ src/components/BucketTypeIcon.tsx
@@ -1,34 +1,51 @@
 import React from 'react';
 
 import { OverlayTrigger, Tooltip } from 'react-bootstrap';
-import { GoVerified, GoUnverified } from 'react-icons/go';
+import { GoVerified, GoUnverified, GoStar } from 'react-icons/go';
 
 import { DELAY_TOOLTIP } from '../constants';
 
 const OFFICIAL_ICON_COLOR = '#2E86C1';
 const COMMUNITY_ICON_COLOR = '#CCCCCC';
+const POPULAR_COMMUNITY_ICON_COLOR = '#6B9DBF';
+const POPULAR_REPOSITORY_THRESHOLD = 50;
 
 type BucketTypeIconProps = {
   official: boolean;
+  stars?: number;
   showTooltip?: boolean;
-};
+} & React.HTMLAttributes<SVGElement>;
 
 const BucketTypeIcon = (props: BucketTypeIconProps): JSX.Element => {
-  const { official, showTooltip = true } = props;
+  const { official, stars = 0, showTooltip = true } = props;
   return (
     <OverlayTrigger
       placement="auto"
       delay={DELAY_TOOLTIP}
       overlay={
-        showTooltip ? <Tooltip id="tooltip">{official ? 'Known bucket' : 'Community bucket'}</Tooltip> : <span />
+        showTooltip ? (
+          <Tooltip id="buckettype-tooltip">
+            <span>
+              {official
+                ? 'Official bucket'
+                : stars >= POPULAR_REPOSITORY_THRESHOLD
+                ? 'Popular community bucket'
+                : 'Community bucket'}
+            </span>
+            <span className="ms-1">
+              ({stars} <GoStar />)
+            </span>
+          </Tooltip>
+        ) : (
+          <span />
+        )
       }
     >
       <span>
-        {official ? (
-          <GoVerified className="ms-1 faIconVerticalAlign" color={OFFICIAL_ICON_COLOR} />
-        ) : (
-          <GoUnverified className="ms-1 faIconVerticalAlign" color={COMMUNITY_ICON_COLOR} />
-        )}
+        {(official && <GoVerified {...props} color={OFFICIAL_ICON_COLOR} />) ||
+          (stars >= POPULAR_REPOSITORY_THRESHOLD && (
+            <GoUnverified {...props} color={POPULAR_COMMUNITY_ICON_COLOR} />
+          )) || <GoUnverified {...props} color={COMMUNITY_ICON_COLOR} />}
       </span>
     </OverlayTrigger>
   );
