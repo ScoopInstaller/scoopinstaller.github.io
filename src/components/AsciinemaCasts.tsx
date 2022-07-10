29ba9b0bf565f7f590e3e49aef48b6223b5405a9
Gregoire Pailler <gregoire@pailler.fr>
2022-07-10 22:19:52 +0800

    #24 Add light theme for asciinema casts

--- src/components/AsciinemaCasts.tsx
+++ src/components/AsciinemaCasts.tsx
@@ -1,8 +1,11 @@
-import React, { useState, useEffect, useRef } from 'react';
+import React, { useContext, useState, useEffect, useRef } from 'react';
 
 import 'asciinema-player/dist/bundle/asciinema-player.css';
+import './asciinema-customthemes.css';
 import * as AsciinemaPlayer from 'asciinema-player';
 import { Nav } from 'react-bootstrap';
+
+import { ColorSchemeContext } from '../colorscheme/ColorSchemeContext';
 
 export type AsciinemaCastItem = {
   key: string;
@@ -15,6 +18,7 @@ type AsciinemaCastsProps = {
 };
 
 const AsciinemaCasts = (props: AsciinemaCastsProps): JSX.Element => {
+  const { isDarkMode } = useContext(ColorSchemeContext);
   const { casts } = props;
 
   const asciiPlayerDivRef = useRef<HTMLDivElement>(null);
@@ -28,10 +32,11 @@ const AsciinemaCasts = (props: AsciinemaCastsProps): JSX.Element => {
         autoPlay: true,
         rows: 15,
         cols: 80,
+        theme: isDarkMode ? 'asciinema' : 'asciinema-light',
       };
       asciiPlayerRef.current = AsciinemaPlayer.create(currentCast.url, asciiPlayerDivRef.current, options);
     }
-  }, [currentCast]);
+  }, [currentCast, isDarkMode]);
 
   return (
     <>
