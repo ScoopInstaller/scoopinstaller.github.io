38ac920146441190824ccfdd1ac3f64182c0095c
unworthyEnzyme <unworthy_enzyme@protonmail.com>
2022-12-14 20:30:21 +0300

    default port should be 3000 for cors

--- vite.config.ts
+++ vite.config.ts
@@ -4,4 +4,10 @@ import { defineConfig } from 'vite';
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
+  server: {
+    port: 3000,
+  },
+  preview: {
+    port: 3000,
+  },
 });
