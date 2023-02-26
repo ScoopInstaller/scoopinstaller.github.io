860a040031750308168e0e5b5317f2376b100b28
Grégoire Pailler <gregoire@pailler.fr>
2023-02-26 08:27:02 +0000

    #51 Replace deprecated document.execCommand('copy')

--- src/components/CopyToClipboardHandler.tsx
+++ src/components/CopyToClipboardHandler.tsx
@@ -1,4 +1,4 @@
-import React, { useRef, useEffect } from 'react';
+import React, { useEffect } from 'react';
 
 type CopyToClipboardHandlerProps = {
   content?: string;
@@ -6,24 +6,21 @@ type CopyToClipboardHandlerProps = {
 };
 
 const CopyToClipboardHandler = (props: CopyToClipboardHandlerProps): JSX.Element => {
-  const textAreaRef = useRef<HTMLTextAreaElement>(null);
   const { content, onContentCopied } = props;
 
   useEffect(() => {
-    if (content) {
-      const textArea = textAreaRef.current;
-      if (textArea) {
-        textArea.value = content;
-        textArea.select();
-        document.execCommand('copy');
-        textArea.blur();
+    const copyClipboardAsync = async (data: string) => {
+      await navigator.clipboard.writeText(data);
+    };
 
-        onContentCopied();
-      }
+    if (content) {
+      copyClipboardAsync(content)
+        .then(() => onContentCopied())
+        .finally(() => {});
     }
   });
 
-  return <textarea ref={textAreaRef} readOnly style={{ position: 'absolute', left: -9999 }} />;
+  return <></>;
 };
 
 export default React.memo(CopyToClipboardHandler);
